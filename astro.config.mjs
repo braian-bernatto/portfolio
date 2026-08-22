c0203ca8d7784fb45cf4c463c756c2301816de8b
braian <braian5353@gmail.com>
2026-08-22 10:44:21 -0300

    chore: set site domain to bernatto.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://braianbernatto.com',
+  site: 'https://bernatto.com',
   integrations: [
     react(),
     sitemap({
